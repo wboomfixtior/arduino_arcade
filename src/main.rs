fa4f0271536816821403f7ea3c6edd7f419edf6b
jeffrey <jcouch210@gmail.com>
2026-02-20 22:44:35 -0900

    Add a library that sort of handles the display

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,55 @@
 #![no_main]
 
 pub mod characters;
-pub mod display;
 pub mod overworld;
 
+use ag_lcd::{Blink, Cursor, Display, LcdDisplay, Lines};
 use panic_halt as _;
 // use ufmt::uWrite;
-
-use crate::display::LCDDisplay;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let delay = arduino_hal::Delay::new();
     // let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    let mut display = LCDDisplay {
-        register_select: pins.d12.into_output(),
-        read_write: pins.d10.into_output(),
-        enable: pins.d11.into_output(),
-        data_0: pins.d5.into_output(),
-        data_1: pins.d4.into_output(),
-        data_2: pins.d3.into_output(),
-        data_3: pins.d2.into_output(),
-    };
+    let rs = pins.d12.into_output().downgrade();
+    let rw = pins.d10.into_output().downgrade();
+    let en = pins.d11.into_output().downgrade();
 
-    // display.set_characters(&characters::CHARACTERS);
-    display.begin();
+    let d4 = pins.d5.into_output().downgrade();
+    let d5 = pins.d4.into_output().downgrade();
+    let d6 = pins.d3.into_output().downgrade();
+    let d7 = pins.d2.into_output().downgrade();
 
-    // display.set_text_cursor(0);
-    // display.write_data(b'H');
-    // display.write_data(b'i');
-    // display.write_data(b'!');
+    let mut lcd = LcdDisplay::new(rs, en, delay)
+        .with_half_bus(d4, d5, d6, d7)
+        .with_rw(rw)
+        .with_display(Display::On)
+        .with_blink(Blink::Off)
+        .with_cursor(Cursor::Off)
+        .with_lines(Lines::TwoLines)
+        .build();
+
+    for (i, &character) in characters::CHARACTERS.iter().enumerate() {
+        lcd.set_character(i as u8, character);
+    }
 
     loop {
+        lcd.clear();
+
+        lcd.set_position(0, 0);
+        lcd.print("Hello world!");
+        lcd.write(0);
+
+        let mut counter = 0;
+
+        lcd.set_position(4, 1);
+        for _ in 0..8 {
+            lcd.write(counter);
+            counter += 1;
+        }
         arduino_hal::delay_ms(1000);
     }
 }
