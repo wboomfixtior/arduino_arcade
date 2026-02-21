281f451bb358f105167a6ae7e0de439122d5ce4c
jeffrey <jcouch210@gmail.com>
2026-02-20 18:49:50 -0900

    Copy arcade

---
+++ src/overworld.rs
@@ -0,0 +1,11 @@
+#[rustfmt::skip]
+pub const ARCADE: [[&[u8]; 2]; 2] = [
+    [
+        b"  \0 [1]     {3} ",
+        b"        (2)     ",
+    ],
+    [
+        b"     \x7f5\x7e     \xff7\xff",
+        b" :4:     <6>    ",
+    ]
+];
