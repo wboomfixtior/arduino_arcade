2e240b01a8b9628eefd9e8809993c372b0d8e8de
jeffrey <jcouch210@gmail.com>
2026-02-21 15:12:41 -0900

    Generalize lcd formatting

--- src/lcd/format.rs
+++ src/lcd/format.rs
@@ -5,9 +5,33 @@ use crate::lcd::LCD;
 impl<RS: PinOps, RW: PinOps, E: PinOps, D4: PinOps, D5: PinOps, D6: PinOps, D7: PinOps>
     LCD<RS, RW, E, D4, D5, D6, D7>
 {
-    pub fn print(&mut self, text: &str) {
-        for byte in text.bytes() {
-            self.write(byte);
+    pub fn print(&mut self, value: impl Display) {
+        let _ = write!(self.fmt(), "{value}");
+    }
+
+    pub fn fmt(&'_ mut self) -> FnWriter<impl FnMut(u8) + '_> {
+        FnWriter::new(|byte| self.write(byte))
+    }
+}
+
+use core::fmt::{self, Display, Write};
+
+#[derive(Debug)]
+pub struct FnWriter<F: FnMut(u8)> {
+    function: F,
+}
+
+impl<F: FnMut(u8)> FnWriter<F> {
+    pub fn new(function: F) -> Self {
+        FnWriter { function }
+    }
+}
+
+impl<F: FnMut(u8)> Write for FnWriter<F> {
+    fn write_str(&mut self, string: &str) -> fmt::Result {
+        for byte in string.bytes() {
+            (self.function)(byte);
         }
+        Ok(())
     }
 }
